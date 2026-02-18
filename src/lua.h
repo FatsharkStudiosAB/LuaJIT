a279cb44cdbde63c95acf7306625f69e94fab067
manuel.blanc <manuel.blanc@fatshark.se>
2026-02-18 09:02:02 +0100

    Fix natvis depending on host application.

--- src/lua.h
+++ src/lua.h
@@ -406,4 +406,6 @@ struct lua_Debug {
 LUA_API lua_State *(luaFS_newstate) (lua_Alloc f, void *ud);
 LUA_API int (luaFS_sizeof) (lua_State *L, int idx);
 
+typedef struct luaFS_lightuserdata { char data[1]; } luaFS_lightuserdata;
+
 #endif
